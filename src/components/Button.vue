647e4dff29e690cfc2f1786a1bdf75743475b345
Meri <smt5783@gmail.com>
2025-11-19 17:16:55 +0800

    feat(components): 建立 Button 元件

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ElButton } from "element-plus";
+import type { ButtonType } from "element-plus";
+import type { Component } from "vue";
+
+// ButtonProps 定義
+defineProps<{
+  type?: ButtonType;
+  size?: "large" | "default" | "small";
+  plain?: boolean;
+  round?: boolean;
+  circle?: boolean;
+  text?: boolean;
+  bg?: boolean;
+  link?: boolean;
+  icon?: string | Component;
+  loading?: boolean;
+  disabled?: boolean;
+  autoInsertSpace?: boolean;
+  nativeType?: "button" | "submit" | "reset";
+}>();
+</script>
+
+<template>
+  <!-- 包裹 slot，支持自定義按鈕內容，並將所有 props 傳遞給 el-button -->
+  <el-button v-bind="$props">
+    <slot />
+  </el-button>
+</template>
