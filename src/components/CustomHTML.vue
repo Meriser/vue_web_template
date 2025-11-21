a04336540952536bb970c9316aed37f309f420dc
Meri <smt5783@gmail.com>
2025-11-21 18:37:44 +0800

    feat(components): 建立 CustomHTML 元件

---
+++ src/components/CustomHTML.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+// 定義 Props
+interface Props {
+  tag?: keyof HTMLElementTagNameMap;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  tag: "div",
+});
+
+// 計算 tag 名稱
+const tagName = computed(() => props.tag);
+</script>
+
+<template>
+  <component :is="tagName">
+    <slot />
+  </component>
+</template>
