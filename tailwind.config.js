d208ed64e0a09ac89ffba2598f3f09d44f9908c2
Meri <smt5783@gmail.com>
2026-01-12 16:32:28 +0800

    feat: 更新 tailwind config 顏色配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html", 
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#1976D2",
-        secondary: "#424242",
-        info: "#2196F3",
-        success: "#4CAF50",
-        warning: "#FFC107",
-        danger: "#FF5252",
-        light: "#F5F5F5",
-        dark: "#212121",
+        // 全大寫 DEFAULT 作為預設值，使用時僅需 text-primary 即可
+        primary: {
+          DEFAULT: "#1976D2",
+        },
+        secondary: {
+          DEFAULT: "#424242",
+        },
+        info: {
+          DEFAULT: "#2196F3",
+        },
+        success: {
+          DEFAULT: "#4CAF50",
+        },
+        warning: {
+          DEFAULT: "#FFC107",
+        },
+        danger: {
+          DEFAULT: "#FF5252",
+        },
+        light: {
+          DEFAULT: "#F5F5F5",
+        },
+        dark: {
+          DEFAULT: "#212121",
+        },
       },
     },
   },
@@ -23,4 +37,4 @@ export default {
   corePlugins: {
     preflight: true,
   },
-}
+};
